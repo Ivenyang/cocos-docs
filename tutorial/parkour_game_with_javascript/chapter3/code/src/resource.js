2d73c474213caa0bbb91bd35837a8d659e8aadd5
u0u0 <lkncastle@gmail.com>
2013-12-26 14:57:43 +0800

    fixed #3514, add source code for chapter 3 of Parkour game.

---
+++ tutorial/parkour_game_with_javascript/chapter3/code/src/resource.js
@@ -0,0 +1,10 @@
+var s_HelloBG = "helloBG.png";
+var s_start_n = "start_n.png";
+var s_start_s = "start_s.png";
+
+var g_resources = [
+    //image
+    {src:s_HelloBG},
+    {src:s_start_n},
+    {src:s_start_s}
+];
