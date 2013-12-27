6bceecc60be126773c5428402608ddc129fb2bb8
IvenYang <yangyong@cocos2d-x.org>
2013-12-27 15:08:17 +0800

    add sample code for chapter6.

---
+++ tutorial/parkour_game_with_javascript/chapter6/code/src/BackgroundLayer.js
@@ -0,0 +1,17 @@
+var BackgroundLayer = cc.Layer.extend({
+    ctor:function () {
+        this._super();
+        this.init();
+    },
+
+    init:function () {
+        this._super();
+
+        var winSize = cc.Director.getInstance().getWinSize();
+
+        var centerPos = cc.p(winSize.width / 2, winSize.height / 2);
+        var spriteBG = cc.Sprite.create(s_PlayBG);
+        spriteBG.setPosition(centerPos);
+        this.addChild(spriteBG);
+    }
+});
