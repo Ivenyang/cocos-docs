31507eef67f7afd2cd84f40f0cdca17d69a3b15b
linshun <linshun@cocos2d-x.org>
2014-05-22 16:39:17 +0800

    Updated chapter 8 to cocos2d-js v3.0

---
+++ tutorial/framework/html5/parkour-game-with-javascript-v3.0/chapter8/code/src/StatusLayer.js
@@ -0,0 +1,25 @@
+var StatusLayer = cc.Layer.extend({
+    labelCoin:null,
+    labelMeter:null,
+    coins:0,
+
+    ctor:function () {
+        this._super();
+        this.init();
+    },
+
+    init:function () {
+        this._super();
+
+        var winsize = cc.director.getWinSize();
+
+        this.labelCoin = cc.LabelTTF.create("Coins:0", "Helvetica", 20);
+        this.labelCoin.setColor(cc.color(0,0,0));//black color
+        this.labelCoin.setPosition(cc.p(70, winsize.height - 20));
+        this.addChild(this.labelCoin);
+
+        this.labelMeter = cc.LabelTTF.create("0M", "Helvetica", 20);
+        this.labelMeter.setPosition(cc.p(winsize.width - 70, winsize.height - 20));
+        this.addChild(this.labelMeter);
+    }
+});
