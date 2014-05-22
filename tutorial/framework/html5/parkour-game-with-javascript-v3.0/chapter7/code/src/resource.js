54f17cd9b73ad9598610d5dc78de7a8ee25815f6
linshun <linshun@cocos2d-x.org>
2014-05-22 15:36:19 +0800

    upated chapter 7 to cocos2d-js v3.0

---
+++ tutorial/framework/html5/parkour-game-with-javascript-v3.0/chapter7/code/src/resource.js
@@ -0,0 +1,24 @@
+var res = {
+    helloBG_png : "res/helloBG.png",
+    start_n_png : "res/start_n.png",
+    start_s_png : "res/start_s.png",
+    PlayBG_png  : "res/PlayBG.png",
+    runner_png  : "res/running.png",
+    runner_plist : "res/running.plist",
+    map_png : "res/map.png",
+    map00_tmx : "res/map00.tmx",
+    map01_tmx : "res/map01.tmx"
+};
+
+var g_resources = [
+    //image
+    res.helloBG_png,
+    res.start_n_png,
+    res.start_s_png,
+    res.PlayBG_png,
+    res.runner_png,
+    res.runner_plist,
+    res.map_png,
+    res.map00_tmx,
+    res.map01_tmx
+];
